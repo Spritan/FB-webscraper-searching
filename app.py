b4d357ad68f1085cb89db49fe44a937ff3df88ba
ritavkashyap123 <ritavkashyap123@gmail.com>
2021-09-27 16:27:29 +0530

    Done

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+html_text = requests.get('https://www.facebook.com/hashtag/bihu').text
+
