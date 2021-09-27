79a38ea274836143ae98d18b020aa4ae6b0a929c
Proyash Paban Sarma Borah <47718224+ProyashPSB@users.noreply.github.com>
2021-09-27 16:35:53 +0530

    class issue

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ import requests
 
 html_text = requests.get('https://www.facebook.com/hashtag/bihu').text
 
+soup = BeautifulSoup(html_text, 'lxml')
+
+posts = soup.find_all(class_='rq0escxv l9j0dhe7 du4w35lb hybvsw6c io0zqebd m5lcvass fbipl8qg nwvqtn77 k4urcfbm ni8dbmo4 stjgntxs sbcfpzgs')
+post = soup.find(class_='rq0escxv l9j0dhe7 du4w35lb hybvsw6c io0zqebd m5lcvass fbipl8qg nwvqtn77 k4urcfbm ni8dbmo4 stjgntxs sbcfpzgs')
+
+print(post)
