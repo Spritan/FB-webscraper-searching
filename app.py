b47c866d629549f84488fa1adeb808f5274b8451
ritavkashyap123 <ritavkashyap123@gmail.com>
2021-09-27 16:50:43 +0530

    Not done

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import requests
 html_text = requests.get('https://www.facebook.com/hashtag/bihu').text
 
 soup = BeautifulSoup(html_text, 'lxml')
-
-posts = soup.find_all(class_='rq0escxv l9j0dhe7 du4w35lb hybvsw6c io0zqebd m5lcvass fbipl8qg nwvqtn77 k4urcfbm ni8dbmo4 stjgntxs sbcfpzgs')
-post = soup.find(class_='rq0escxv l9j0dhe7 du4w35lb hybvsw6c io0zqebd m5lcvass fbipl8qg nwvqtn77 k4urcfbm ni8dbmo4 stjgntxs sbcfpzgs')
+print(soup)
+posts = soup.find_all(class_='rq0escxv l9j0dhe7 du4w35lb qmfd67dx hpfvmrgz gile2uim buofh1pr g5gj957u aov4n071 oi9244e8 bi6gxh9e h676nmdw aghb5jc5')
+post = soup.find(class_='rq0escxv l9j0dhe7 du4w35lb qmfd67dx hpfvmrgz gile2uim buofh1pr g5gj957u aov4n071 oi9244e8 bi6gxh9e h676nmdw aghb5jc5')
 
 print(post)
